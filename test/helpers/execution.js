2cd41bc2135d180f99633fc2134c3460f8a505d9
julianxcarter <juliancarter55@gmail.com>
2021-03-17 09:20:05 -0400

    package-lock.json removed

--- test/helpers/execution.js
+++ test/helpers/execution.js
@@ -6,7 +6,7 @@ const { PatientSource } = require('cql-exec-fhir');
  * @param {Array} elmJSONs array of ELM JSON objects
  * @param {Object} patientBundle patient record to execute against
  * @param {Object} valueSetMap valueSetMap for CodeService
- * @param {Object} libraryID the library ID of the cql library corresponding to the ELM
+ * @param {String} libraryID the library ID of the cql library corresponding to the ELM
  * @returns {Object} cql-execution-results
  */
 function execute(elmJSONs, patientBundle, valueSetMap, libraryID = 'mCODE') {
