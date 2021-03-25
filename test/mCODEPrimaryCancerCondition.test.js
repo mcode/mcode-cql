dbd8dd413fe7b1ec8eb2a50b9a4879db1ffe7571
Saul Kravitz <saul@mitre.org>
2021-03-25 18:33:14 -0400

    CancerPatient, ECOG, Karnofsky, PrimaryCancerCondtion, TNMClinicalStageGroup (start)

---
+++ test/mCODEPrimaryCancerCondition.test.js
@@ -0,0 +1,34 @@
+const { loadELM, loadJSONFixture, loadValueSets } = require('./helpers/fixtureLoader');
+const { mapValueSets } = require('./helpers/valueSetMapper');
+const { execute, setup } = require('./helpers/execution');
+// eslint-disable-next-line import/order
+const { FunctionRef } = require('cql-execution/lib/elm/expressions');
+
+let executionResults;
+let setupResults;
+beforeAll(() => {
+  // Set up necessary data for cql-execution
+  const valueSets = loadValueSets();
+  const valueSetMap = mapValueSets(valueSets);
+  const elm = loadELM();
+  elm.push(loadJSONFixture(__dirname, './fixtures/FHIRHelpers.json'));
+  const patientBundle = loadJSONFixture(__dirname, './fixtures/patients/Bundle-mCODECQLExample01.json');
+
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCODEPrimaryCancerCondition');
+  setupResults = setup('mCODEPrimaryCancerCondition', elm, patientBundle, valueSetMap);
+  console.log(executionResults);
+});
+
+
+test('Can identify Cancer Condition Code', () => {
+    const functionRef = new FunctionRef({
+      type: 'FunctionRef',
+      name: 'mCODEPrimaryCancerCondition',
+      operand: [{ type: 'First', source: { type: 'ExpressionRef', name: 'Cancer Genetic Variants' } }],
+    });
+    const values = functionRef.exec(setupResults.context);
+    // eslint-disable-next-line no-unused-expressions
+    expect(values).not.null;
+    expect(values.length).toBe(1);
+    expect(values[0].coding[0].code.value).toBe('HGNC:11389');
+  });
