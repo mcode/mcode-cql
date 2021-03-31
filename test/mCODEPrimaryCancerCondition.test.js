fce8525b70b9b2120a5fc43984a361042f6d2e32
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-03-31 17:16:10 -0400

    cleanup tests

--- test/mCODEPrimaryCancerCondition.test.js
+++ test/mCODEPrimaryCancerCondition.test.js
@@ -1,33 +1,18 @@
 const { loadELM, loadJSONFixture, loadValueSets } = require('./helpers/fixtureLoader');
 const { mapValueSets } = require('./helpers/valueSetMapper');
-const { execute, setup } = require('./helpers/execution');
-// eslint-disable-next-line import/order
-const { FunctionRef } = require('cql-execution/lib/elm/expressions');
+const { execute } = require('./helpers/execution');
 
 let executionResults;
-let setupResults;
 beforeAll(() => {
   // Set up necessary data for cql-execution
   const valueSets = loadValueSets();
   const valueSetMap = mapValueSets(valueSets);
   const elm = loadELM();
-  elm.push(loadJSONFixture(__dirname, './fixtures/FHIRHelpers.json'));
+
   const patientBundle = loadJSONFixture(__dirname, './fixtures/patients/Bundle-mCODECQLExample01.json');
 
-  executionResults = execute(elm, patientBundle, valueSetMap, 'mCODEPrimaryCancerCondition');
-  setupResults = setup('mCODEPrimaryCancerCondition', elm, patientBundle, valueSetMap);
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCODE');
   console.log(executionResults);
 });
 
-test('Can identify Cancer Condition Code', () => {
-  const functionRef = new FunctionRef({
-    type: 'FunctionRef',
-    name: 'mCODEPrimaryCancerCondition',
-    operand: [{ type: 'First', source: { type: 'ExpressionRef', name: 'Cancer Genetic Variants' } }],
-  });
-  const values = functionRef.exec(setupResults.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].coding[0].code.value).toBe('HGNC:11389');
-});
+test('Can identify PrimaryCancerConditions', () => {});
