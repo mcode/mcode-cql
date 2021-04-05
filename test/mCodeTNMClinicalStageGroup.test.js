be14f6225b721bb617af3835249e156aa20db41c
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-04-05 12:59:41 -0400

    update clinical stage tests

--- test/mCodeTNMClinicalStageGroup.test.js
+++ test/mCodeTNMClinicalStageGroup.test.js
@@ -10,25 +10,30 @@ beforeAll(() => {
   const elm = loadELM();
 
   elm.push(loadJSONFixture(__dirname, './fixtures/elm/mCodeTNMClinicalStageGroup.test.json'));
-  const patientBundle = loadJSONFixture(
-    __dirname,
-    './fixtures/patients/mcode-extraction-patient-1.json',
-  );
+  const patientBundle = loadJSONFixture(__dirname, './fixtures/patients/mcode-extraction-patient-1.json');
 
   testSetup = setup('mCodeTNMClinicalStageGroupTest', elm, patientBundle, valueSetMap);
 });
 
+test('Test Reference id', () => {
+  const expr = testSetup.library.expressions['Test Reference id'];
+  const values = expr.exec(testSetup.context);
+
+  expect(values).not.toBeNull();
+  expect(values).toBe('a121869e84dc63c7b6e98b6f299e168a9d212048a1c05ddc8ea06ac01427a74e');
+});
+
 test('Test is TNM Clinical Stage Group', () => {
-  const executionTestResults = testSetup.executor.exec_expression('Test is TNM Clinical Stage Group', testSetup.patientSource);
-  const values = executionTestResults.patientResults['123']['Test is TNM Clinical Stage Group'];
+  const expr = testSetup.library.expressions['Test is TNM Clinical Stage Group'];
+  const values = expr.exec(testSetup.context);
 
   expect(values).not.toBeNull();
   expect(values).toBe(true);
 });
 
 test('Test TNM Clinical Stage Group Categories', () => {
-  const executionTestResults = testSetup.executor.exec_expression('Test TNM Clinical Stage Group Categories', testSetup.patientSource);
-  const values = executionTestResults.patientResults['123']['Test TNM Clinical Stage Group Categories'];
+  const expr = testSetup.library.expressions['Test TNM Clinical Stage Group Categories'];
+  const values = expr.exec(testSetup.context);
 
   expect(values).not.toBeNull();
   expect(values.length).toBe(3);
