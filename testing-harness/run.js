860c2abb9d3015cfae78752e16381784849ccfd8
Dylan Mendelowitz <dmendelowitz@mitre.org>
2021-04-12 14:43:07 -0400

    chaning to run .bat instead of .ps1

--- testing-harness/run.js
+++ testing-harness/run.js
@@ -4,7 +4,7 @@ const { spawn } = require('child_process');
 
 const args = process.argv.slice(2);
 if (process.platform === 'win32') {
-  spawn('./testing-harness/scripts/run_tests.ps1', args, { shell: 'powershell.exe', stdio: 'inherit' });
+  spawn('testing-harness\\scripts\\run_tests.bat', args, { stdio: 'inherit' });
 } else {
   spawn('./testing-harness/scripts/run_tests.sh', args, { stdio: 'inherit' });
 }
