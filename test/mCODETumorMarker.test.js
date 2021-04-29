11b76e4d2d80257103efee5d31a9fa7eef618507
Matthew Gramigna <mgramigna@mitre.org>
2021-04-29 16:00:16 -0400

    Update TumorMarker to use cql-testing-harness

--- test/mCODETumorMarker.test.js
+++ test/mCODETumorMarker.test.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
-const { setup } = require('./setup');
+const { execute, defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
 
-let testSetup;
+let executionResults;
 beforeAll(() => {
   const valueSets = defaultLoadValuesets();
   const testValueSet = loadJSONFixture(
@@ -14,28 +13,30 @@ beforeAll(() => {
 
   const patientBundle = loadJSONFixture(path.join(__dirname, './fixtures/patients/Bundle-mCODECQLExample01.json'));
 
-  testSetup = setup('mCODETumorMarkerTest', elm, patientBundle, valueSetMap);
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCODETumorMarkerTest');
 });
+
+const patientId = 'mCODECQLExample01';
+
 test('Can Identify Tumor Marker Test', () => {
-  const expr = testSetup.library.expressions['Test Tumor Marker'];
-  const values = expr.exec(testSetup.context);
-  expect(values.id.value).toBe('53a6e3e8-fbcd-4cdf-8e8f-28de9ec5d234');
+  const expr = executionResults.patientResults[patientId]['Test Tumor Marker'];
+  expect(expr.id.value).toBe('53a6e3e8-fbcd-4cdf-8e8f-28de9ec5d234');
 });
+
 test('Can identify Tumor Markers By Value Set', () => {
-  const expr = testSetup.library.expressions['Test Tumor Markers By Value Set'];
-  const values = expr.exec(testSetup.context);
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(2);
+  const expr = executionResults.patientResults[patientId]['Test Tumor Markers By Value Set'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(2);
 });
+
 test('Can identify Tumor Marker Code', () => {
-  const expr = testSetup.library.expressions['Test Tumor Marker Code'];
-  const values = expr.exec(testSetup.context);
-  expect(values).not.toBeNull();
-  expect(values.coding[0].code.value).toBe('48676-1');
+  const expr = executionResults.patientResults[patientId]['Test Tumor Marker Code'];
+  expect(expr).not.toBeNull();
+  expect(expr.coding[0].code.value).toBe('48676-1');
 });
+
 test('Can identify Tumor Marker Value', () => {
-  const expr = testSetup.library.expressions['Test Tumor Marker Data Value'];
-  const values = expr.exec(testSetup.context);
-  expect(values).not.toBeNull();
-  expect(values.coding[0].code.value).toBe('260385009');
+  const expr = executionResults.patientResults[patientId]['Test Tumor Marker Data Value'];
+  expect(expr).not.toBeNull();
+  expect(expr.coding[0].code.value).toBe('260385009');
 });
