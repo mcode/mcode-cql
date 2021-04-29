c56a2d1b0b40fe49e491d444cd6089c4cecfa3e6
Julia Afeltra <jafeltra@mitre.org>
2021-04-29 16:57:35 -0400

    Update TNM Pathologic Stage group test

--- test/mCodeTNMPathologicalStageGroup.test.js
+++ test/mCodeTNMPathologicalStageGroup.test.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
-const { setup } = require('./setup');
+const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets, execute } = require('cql-testing-harness');
 
-let testSetup;
+let executionResults;
 beforeAll(() => {
   // Set up necessary data for cql-execution
   const valueSets = defaultLoadValuesets();
@@ -11,71 +10,57 @@ beforeAll(() => {
 
   const patientBundle = loadJSONFixture(path.join(__dirname, './fixtures/patients/mcode-extraction-patient-1.json'));
 
-  testSetup = setup('mCodeTNMPathologicalStageGroupTest', elm, patientBundle, valueSetMap);
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCodeTNMPathologicalStageGroupTest');
 });
 
-test('Test Is TNM Pathological Stage Group', () => {
-  const expr = testSetup.library.expressions['Test Is TNM Pathological Stage Group'];
-  const values = expr.exec(testSetup.context);
+const patientId = '123';
 
-  expect(values).not.toBeNull();
-  expect(values).toBe(true);
+test('Test Is TNM Pathological Stage Group', () => {
+  const expr = executionResults.patientResults[patientId]['Test Is TNM Pathological Stage Group'];
+  expect(expr).not.toBeNull();
+  expect(expr).toBe(true);
 });
 
 test('Test Is TNM Pathological Stage Group False', () => {
-  const expr = testSetup.library.expressions['Test Is TNM Pathological Stage Group False'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values).toBe(false);
+  const expr = executionResults.patientResults[patientId]['Test Is TNM Pathological Stage Group False'];
+  expect(expr).not.toBeNull();
+  expect(expr).toBe(false);
 });
 
 test('Test Current TNM Pathological Stage Groups', () => {
-  const expr = testSetup.library.expressions['Test Current TNM Pathological Stage Groups'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalStageGroupExample01');
+  const expr = executionResults.patientResults[patientId]['Test Current TNM Pathological Stage Groups'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalStageGroupExample01');
 });
 
 test('Test Latest TNM Pathological Stage Group', () => {
-  const expr = testSetup.library.expressions['Test Latest TNM Pathological Stage Group'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.id.value).toBe('mCODETNMPathologicalStageGroupExample02');
+  const expr = executionResults.patientResults[patientId]['Test Latest TNM Pathological Stage Group'];
+  expect(expr).not.toBeNull();
+  expect(expr.id.value).toBe('mCODETNMPathologicalStageGroupExample02');
 });
 
 test('Test Earliest TNM Pathological Stage Group', () => {
-  const expr = testSetup.library.expressions['Test Earliest TNM Pathological Stage Group'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.id.value).toBe('mCODETNMPathologicalStageGroupExample01');
+  const expr = executionResults.patientResults[patientId]['Test Earliest TNM Pathological Stage Group'];
+  expect(expr).not.toBeNull();
+  expect(expr.id.value).toBe('mCODETNMPathologicalStageGroupExample01');
 });
 
 test('Test TNM Pathological Stage Group Categories', () => {
-  const expr = testSetup.library.expressions['Test TNM Pathological Stage Group Categories'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(3);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
+  const expr = executionResults.patientResults[patientId]['Test TNM Pathological Stage Group Categories'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(3);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
 });
 
 test('Test TNM Pathological Stage Group Value', () => {
-  const expr = testSetup.library.expressions['Test TNM Pathological Stage Group Value'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.coding[0].code.value).toBe('3C');
+  const expr = executionResults.patientResults[patientId]['Test TNM Pathological Stage Group Value'];
+  expect(expr).not.toBeNull();
+  expect(expr.coding[0].code.value).toBe('3C');
 });
 
 test('Test TNM Pathological Stage Group Staging System', () => {
-  const expr = testSetup.library.expressions['Test TNM Pathological Stage Group Staging System'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.coding[0].code.value).toBe('C146985');
+  const expr = executionResults.patientResults[patientId]['Test TNM Pathological Stage Group Staging System'];
+  expect(expr).not.toBeNull();
+  expect(expr.coding[0].code.value).toBe('C146985');
 });
