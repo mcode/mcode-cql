23de4f92efd41e7f7d6dd8ef109f5582ec342067
Julia Afeltra <jafeltra@mitre.org>
2021-04-29 16:54:54 -0400

    Update TNM Pathologic Tumor test

--- test/mCodeTNMPathologicalPrimaryTumorCategory.test.js
+++ test/mCodeTNMPathologicalPrimaryTumorCategory.test.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
-const { setup } = require('./setup');
+const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets, execute } = require('cql-testing-harness');
 
-let testSetup;
-let mCodeTestSetup;
+let executionResults;
+let mCODEExecutionResults;
 beforeAll(() => {
   // Set up necessary data for cql-execution
   const valueSets = defaultLoadValuesets();
@@ -11,57 +10,48 @@ beforeAll(() => {
   const elm = defaultLoadElm();
 
   const patientBundle = loadJSONFixture(path.join(__dirname, './fixtures/patients/mcode-extraction-patient-1.json'));
-  mCodeTestSetup = setup('mCODE', elm, patientBundle, valueSetMap);
-  testSetup = setup('mCodeTNMPathologicalPrimaryTumorCategoryTest', elm, patientBundle, valueSetMap);
+  mCODEExecutionResults = execute(elm, patientBundle, valueSetMap, 'mCODE');
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCodeTNMPathologicalPrimaryTumorCategoryTest');
 });
 
+const patientId = '123';
+
 test('Test Observation', () => {
-  const expr = testSetup.library.expressions['Test Observation'];
-  const values = expr.exec(testSetup.context);
-  expect(values).not.toBeNull();
-  expect(values.id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
+  const expr = executionResults.patientResults[patientId]['Test Observation'];
+  expect(expr).not.toBeNull();
+  expect(expr.id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
 });
 
 test('Test TNM Pathological Primary Tumor Categories', () => {
-  const expr = mCodeTestSetup.library.expressions['TNM Pathological Primary Tumor Categories'];
-  const values = expr.exec(mCodeTestSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(2);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
+  const expr = mCODEExecutionResults.patientResults[patientId]['TNM Pathological Primary Tumor Categories'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(2);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
 });
 
 test('Test Is TNM Pathological Primary Tumor Category', () => {
-  const expr = testSetup.library.expressions['Test Is TNM Pathological Primary Tumor Category'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values).toBe(true);
+  const expr = executionResults.patientResults[patientId]['Test Is TNM Pathological Primary Tumor Category'];
+  expect(expr).not.toBeNull();
+  expect(expr).toBe(true);
 });
 
 test('Test Current TNM Pathological Primary Tumor Categories', () => {
-  const expr = testSetup.library.expressions['Test Current TNM Pathological Primary Tumor Categories'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
+  const expr = executionResults.patientResults[patientId]['Test Current TNM Pathological Primary Tumor Categories'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
 });
 
 test('Test Latest TNM Pathological Primary Tumor Categories', () => {
-  const expr = testSetup.library.expressions['Test Latest TNM Pathological Primary Tumor Categories'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample02');
+  const expr = executionResults.patientResults[patientId]['Test Latest TNM Pathological Primary Tumor Categories'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample02');
 });
 
 test('Test Earliest TNM Pathological Primary Tumor Categories', () => {
-  const expr = testSetup.library.expressions['Test Earliest TNM Pathological Primary Tumor Categories'];
-  const values = expr.exec(testSetup.context);
-
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
+  const expr = executionResults.patientResults[patientId]['Test Earliest TNM Pathological Primary Tumor Categories'];
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('mCODETNMPathologicalPrimaryTumorCategoryExample01');
 });
