4ba968c35c142d775fb9e9ca01e1fae9122a15e1
Matthew Gramigna <mgramigna@mitre.org>
2021-04-29 16:51:22 -0400

    Update TNM Distant Metastases Categories to use cql-testing-harness

--- test/mCodeTNMPathologicalDistantMetastasesCategory.test.js
+++ test/mCodeTNMPathologicalDistantMetastasesCategory.test.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const { defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
-const { setup } = require('./setup');
+const { execute, defaultLoadElm, loadJSONFixture, defaultLoadValuesets, mapValueSets } = require('cql-testing-harness');
 
-let mCodeTestSetup;
-let testSetup;
+let mCODEExecutionResults;
+let executionResults;
 beforeAll(() => {
   // Set up necessary data for cql-execution
   const valueSets = defaultLoadValuesets();
@@ -12,41 +11,39 @@ beforeAll(() => {
 
   const patientBundle = loadJSONFixture(path.join(__dirname, './fixtures/patients/mcode-extraction-patient-1.json'));
 
-  mCodeTestSetup = setup('mCODE', elm, patientBundle, valueSetMap);
-  testSetup = setup('mCodeTNMClinicalDistantMetastasesCategoryTest', elm, patientBundle, valueSetMap);
+  mCODEExecutionResults = execute(elm, patientBundle, valueSetMap, 'mCODE');
+  executionResults = execute(elm, patientBundle, valueSetMap, 'mCodeTNMClinicalDistantMetastasesCategoryTest');
 });
 
-test('Test TNM Clinical Distant Metastases Categories', () => {
-  const expr = mCodeTestSetup.library.expressions['TNM Clinical Distant Metastases Categories'];
-  const values = expr.exec(mCodeTestSetup.context);
+const patientId = '123';
 
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(2);
-  expect(values[1].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3-2');
+test('Test TNM Clinical Distant Metastases Categories', () => {
+  const expr = mCODEExecutionResults.patientResults[patientId]['TNM Clinical Distant Metastases Categories'];
+
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(2);
+  expect(expr[1].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3-2');
 });
 
 test('Test Is TNM Clinical Distant Metastases Category', () => {
-  const expr = testSetup.library.expressions['Test Is TNM Clinical Distant Metastases Category'];
-  const values = expr.exec(testSetup.context);
+  const expr = executionResults.patientResults[patientId]['Test Is TNM Clinical Distant Metastases Category'];
 
-  expect(values).not.toBeNull();
-  expect(values).toBe(true);
+  expect(expr).not.toBeNull();
+  expect(expr).toBe(true);
 });
 
 test('Test Current TNM Clinical Distant Metastases Categories', () => {
-  const expr = testSetup.library.expressions['Test Current TNM Clinical Distant Metastases Categories'];
-  const values = expr.exec(testSetup.context);
+  const expr = executionResults.patientResults[patientId]['Test Current TNM Clinical Distant Metastases Categories'];
 
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3');
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3');
 });
 
 test('Test Latest TNM Clinical Distant Metastases Categories', () => {
-  const expr = testSetup.library.expressions['Test Latest TNM Clinical Distant Metastases Categories'];
-  const values = expr.exec(testSetup.context);
+  const expr = executionResults.patientResults[patientId]['Test Latest TNM Clinical Distant Metastases Categories'];
 
-  expect(values).not.toBeNull();
-  expect(values.length).toBe(1);
-  expect(values[0].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3');
+  expect(expr).not.toBeNull();
+  expect(expr.length).toBe(1);
+  expect(expr[0].id.value).toBe('72f00994860c396a7905153220ad183b16740c840b3719b46bb200c2061864a3');
 });
